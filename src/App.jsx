f570295256f76e72641ee32e5336e63ccecc80be
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-08 16:28:31 +1000

    handle add todos

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import { Tabs } from "./components/Tabs"
 import { TodoInput } from "./components/TodoInput"
 import { TodoList } from "./components/TodoList"
 
+import { useState } from 'react'
+
 function App() {
-  const todos = [
-    {input: 'Hello! Add your first todo!', complete: true},
-    {input: 'Get the groceries!', complete: false},
-    {input: 'Learn how to web design', complete: false},
-    {input: 'Say hi to gran', complete: true},
-  ]
+  const [todos, setTodos] = useState([
+    {input: "Hello! Add your first todo!", complete: true}
+  ])
+
+  function handleAddTodo(newTodo) {
+    const newTodoList = [...todos, {input: newTodo, complete: false}]
+    setTodos(newTodoList)
+  }
 
   return (
     <>
       <Header todos={todos} />
       <Tabs todos={todos} />
-      <TodoInput todos={todos} />
       <TodoList todos={todos}/>
+      <TodoInput handleAddTodo={handleAddTodo} />
     </>
   )
 }
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,5 +1,23 @@
-export function TodoInput() {
+import { useState } from 'react'
+
+export function TodoInput(props) {
+    const { handleAddTodo } = props
+    const [inputValue, setInputValue] = useState('')
+
     return (
-        <div></div>
+        <div className="input-container">
+            <input
+                value={inputValue}
+                onChange={(e) => {setInputValue(e.target.value)}}
+                placeholder="Add task"
+            />
+            <button onClick={() => {
+                if (!inputValue) { return }
+                handleAddTodo(inputValue);
+                setInputValue('');
+            }}>
+            <i className="fa-solid fa-plus"></i>
+            </button>
+        </div>
     )
 }
